8bc0858bb0329c2ae65b1ad9e7b31362aa9cb4b1
dustinsnoap <afrobacon@gmail.com>
2020-04-13 01:31:57 -0400

    added global style

--- minimalist/src/index.js
+++ minimalist/src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom'
 
 //views
 //style
+import GlobalStyle from './style/global'
 //init
 
 ReactDOM.render(
     <div>
+        <GlobalStyle/>
         <h1>Portfolio: Minimalist</h1>
     </div>,
     root
