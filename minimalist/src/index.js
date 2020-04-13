c5c20de50c6ee977a1c015a58f3f68478fddbd5e
dustinsnoap <afrobacon@gmail.com>
2020-04-13 01:24:28 -0400

    created portfolio/minimalist

---
+++ minimalist/src/index.js
@@ -0,0 +1,14 @@
+//imports
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//views
+//style
+//init
+
+ReactDOM.render(
+    <div>
+        <h1>Portfolio: Minimalist</h1>
+    </div>,
+    root
+)
