af25b19af97a79e097cf4ad5e1aad85a236a9358
dustinsnoap <afrobacon@gmail.com>
2020-06-27 19:39:04 -0400

    mobile -portfolio element switches between landscape and portrait depending on the ratio between height and width of the window

--- src/views/portfolio/index.js
+++ src/views/portfolio/index.js
@@ -11,14 +11,30 @@ class Portfolio extends Component {
     constructor() {
         super()
         this.state = {
-            current_page: 2,
+            current_page: 0,
             pages: ['intro','aboutme','projects','contact']
         }
     }
+    componentDidMount() {
+        this.update_screen_orientation()
+        window.addEventListener('resize',this.update_screen_orientation)
+    }
     change_page = page_num => {
-        this.setState(() => ({
-            current_page: page_num
-        }))
+        this.setState(() => ({current_page: page_num}))
+    }
+    update_screen_orientation = () => {
+        const portfolio_element = document.querySelector('.portfolio')
+        const screen_type = this.get_screen_type()
+        if(screen_type === 'landscape') {
+            portfolio_element.classList.remove('portrait')
+            portfolio_element.classList.add('landscape')
+        } else {
+            portfolio_element.classList.remove('landscape')
+            portfolio_element.classList.add('portrait')
+        }
+    }
+    get_screen_type = () => {
+        return window.innerWidth > window.innerHeight ? 'landscape' : 'portrait'
     }
     render = () =>
         <Wrapper className='portfolio'>
