60656ef577bf190e5b78a71289a583a832decdd5
dustinsnoap <dustinsnoap@gmail.com>
2020-07-23 02:52:46 -0400

    styled skills page

--- src/pages/skills/style/portrait.js
+++ src/pages/skills/style/portrait.js
@@ -1,6 +1,40 @@
 export default `
     height: 100vh;
-    width: 100vw;
-    background-color: #786138;
-    font-size: 5vw;
+    // width: 100vw;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    // justify-content: center;
+    padding: 5vw;
+    position: relative;
+    h1 {
+        font-size: 25vw;
+        font-family: orbitron;
+        color: #08f;
+    }
+    .content {
+        width: 100%;
+        .text {
+            p {
+                font-family: neue;
+                text-align: justify;
+                color: #ccc;
+                font-size: 4vw;
+                margin: 2vh 0;
+            }
+        }
+        .skills {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            span {
+                color: #ccc;
+                text-align: center;
+                font-variant: small-caps;
+                font-family: hind;
+                font-size: 4vw;
+                margin: 2vw;
+            }
+        }
+    }
+
 `
